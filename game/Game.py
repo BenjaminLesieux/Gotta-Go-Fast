573b280694e7a7bbeba7afb7bb2a01d7235ae577
stephench420 <benjamin.lesieux@efrei.net>
2020-04-11 18:02:25 +0200

    Quent1 lel
    Signed-off-by: stephench420 <benjamin.lesieux@efrei.net>

--- game/Game.py
+++ game/Game.py
@@ -51,6 +51,8 @@ class Game:
             while lines[j][i] != "-":
                 i += 1
 
+            # Commentaire pour commit
+
             y = int(lines[j][last + 1:i])
             position = (x, y)
             mobile = True if lines[j][i + 1:] == "True" else False
