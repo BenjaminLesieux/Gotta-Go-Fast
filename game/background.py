f9e553ecd325c9910973acb8b20242a0c4f8ebf4
Faust1-2 <faustin.dewas@gmail.com>
2020-05-31 16:19:25 +0200

    Com background

--- game/background.py
+++ game/background.py
@@ -8,7 +8,7 @@ class Background():
     def __init__(self):
         
         self.bck_ground = [pygame.transform.scale(pygame.image.load("images/Background1.jpg"),(1280, 1200)), pygame.transform.scale(pygame.image.load("images/Background2.jpg"),(1280, 1200))]
-
+        #Les fonds d'écrans sont plus longs que la fenêtre pour donner un véritable effet de longueur.
         self.pos_1 = -300
         self.pos_2 = -1500
         self.limit = [900, -300]  # bas, haut
@@ -18,6 +18,12 @@ class Background():
         self.n_screen = 3
         self.sens = 1
 
+    """
+        Deux directions up et down pour le leveleditor
+        n_screen est le nombre maximal d'écrans pouvant défiler
+        Si écran 1 sort de l'écran, il passe en dessous d'écran 2.
+        Même chose pour écran 2.
+    """
     def defil(self, direction):
 
         if direction == "up":
@@ -39,6 +45,9 @@ class Background():
                     self.pos_2 = -1500
                     self.count += 1
     
+    """
+        Affiche les deux background dans la fenêtre
+    """
     def draw(self, surface):
         surface.blit(self.bck_ground[0], (0, self.pos_1))
         surface.blit(self.bck_ground[1], (0, self.pos_2))
